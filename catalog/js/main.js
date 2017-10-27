8ebea60be13586f439c5ab4f56d5c28f1236e984
Bingogg <bingogg@i.ua>
2017-10-27 05:31:31 +0300

    Add MobileMenu 2 and New SideBar

--- catalog/js/main.js
+++ catalog/js/main.js
@@ -1,12 +1,33 @@
-$(function() {
-    $('.owl-carousel').owlCarousel({
-        items: 1,
-        singleItem: true,
-        nav: true,
-        loop: true,
-        navText: [
-            '<div class="btn btn-primary"> <i class="fa fa-angle-left"></i></div>',
-            '<div class="btn btn-primary"> <i class="fa fa-angle-right"></i></div>'
-        ],
-    })
+$( document ).ready(function() {
+   $(function() {
+        $('.owl-carousel').owlCarousel({
+            items: 1,
+            singleItem: true,
+            nav: true,
+            loop: true,
+            navText: [
+                '<div class="btn btn-primary"> <i class="fa fa-angle-left"></i></div>',
+                '<div class="btn btn-primary"> <i class="fa fa-angle-right"></i></div>'
+            ],
+         })
+    });
+   $('.spoiler-menu').click(function() {
+    $('ul.main-nav').toggleClass("active");
+    });   
+   //Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
 });
+
